f4a65e43fee3f23e365f8f3e9c85a058b12dafe7
Leonardo Dos Santos Dias <dleonardo@adhosting.internal>
2022-02-14 14:31:14 -0300

    Adicionando o arquivo etapas.js

---
+++ etapas.js
@@ -0,0 +1,58 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numero: 5, 
+        candidatos: [
+            {
+                numero: 38111, 
+                nome: 'Bill Klynton',
+                partido: 'ABC',
+                fotos: [
+                    {url:'28111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Stiven Jobs',
+                partido: 'Republicanos', 
+                fotos: [
+                    {url:'77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {  
+        titulo: 'PREFEITO',
+        numeros: 2, 
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ben 10', 
+                partido: 'ABC', 
+                vice: 'Cic', 
+                fotos : [
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            
+            {
+                numero: 84,
+                nome: 'Zulano',
+                partido: 'QWERTY', 
+                vice: 'Zul', 
+                fotos: [
+                    {url:'84.jpg', legenda: 'Prefeito'},
+                    {url:'84_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },        
+        ]
+    }
+
+
+
+
+
+
+
+]
