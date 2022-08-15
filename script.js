409fd47234a15752c8ffb72d211c378fca55b726
leonardo.dias <leonardo.dias@movemais.com>
2022-08-15 11:04:16 -0300

    Atualizando projeto

--- script.js
+++ script.js
@@ -5,42 +5,29 @@ let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
-let etapaAtual = 0;
+let etapaAtual = 0; 
 let numero = '';
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
 
     let numeroHtml = '';
-    numero = '';
-    votoBranco = false;
 
-    for (let i = 0; i < etapa.numeros; i++) {
-        if (i === 0) {
-            numeroHtml += '<div class="numero pisca"></div>';
-        } else {
-            numeroHtml += '<div class="numero"></div>';
-        }
+    for(let i = 0; i < etapa.numeros; i++) {
+        numeroHtml += '<div class="numero"></div>';
     }
 
-    seuVotoPara.style.display = 'none';
-    cargo.innerHTML = etapa.titulo;
+    seuVotoPara.style.display = 'none'; 
+    cargo.innerHTML = etapa.titulo; 
     descricao.innerHTML = '';
-    aviso.style.display = 'none';
-    lateral.innerHTML = '';
+    aviso.style.display = 'none'; 
+    lateral.innerHTML = ''; 
     numeros.innerHTML = numeroHtml;
-
-}
-function atualizaTierce() {
 
 }
 
 function clicou(n) {
-    let elNumero = document; querySelector('.numero.pisca');
-    if (elNumero != null) {
-        elNumero.innerHTML = n;
-        numero = `${numero}${n}`;
-    }
+    alert('Clicou em ' + n);
 }
 
 function branco() {
@@ -48,7 +35,7 @@ function branco() {
 
 }
 
-function corrige() {
+function corrige() {    
     alert('Clicou em CORRIGE!');
 }
 
